cb8cebe5a6239d464c81c4de0400755176ba01a9
br.Theo <brossiertheo@live.fr>
2022-06-19 23:23:30 +0200

    staged files

--- src/JuiTheme.ts
+++ src/JuiTheme.ts
@@ -1,3 +1,11 @@
+declare global {
+  var __JUI__: {
+    iconProvider: {
+      [key: string]: string
+    }
+  }
+}
+
 export enum ESize {
   xs='1rem', 
   sm='1.5rem', 
@@ -9,3 +17,11 @@ export enum ESize {
   xxxxl='10rem',
 }
 export type ESizeKeys = keyof typeof ESize
+
+if (!globalThis.hasOwnProperty('__JUI__')) 
+  globalThis.__JUI__ = { 
+    iconProvider: { 
+      default: 'https://raw.githubusercontent.com/Templarian/MaterialDesign-SVG/master/svg/' 
+    } 
+  }
+globalThis.__JUI__.iconProvider.default = 'https://raw.githubusercontent.com/Templarian/MaterialDesign-SVG/master/svg/'
